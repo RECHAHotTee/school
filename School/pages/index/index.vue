33c00e0cd18464372f37ec194a0cc61f292014c4
RECHAHotTee <50287648+RECHAHotTee@users.noreply.github.com>
2023-01-30 00:15:55 +0800

    Add files via upload

---
+++ School/pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="content">
+		<text class="text-center">欢迎登陆小程序</text>
+		<button @click="goToLogin">goToLogin</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			goToLogin(){
+				uni.navigateTo({
+					url: '/pages/login/login'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ School/pages/relax/relax.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="">
+		relax
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
