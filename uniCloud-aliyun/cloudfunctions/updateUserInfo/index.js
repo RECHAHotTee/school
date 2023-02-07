b2d4add217649580d2d6e02613809119a0a81b87
dongzeping1 <dongzeping1@jd.com>
2023-02-07 18:09:25 +0800

    feat: 用户信息修改接口初始化

---
+++ uniCloud-aliyun/cloudfunctions/updateUserInfo/index.js
@@ -0,0 +1,9 @@
+'use strict';
+exports.main = async (event, context) => {
+	//1.头像，姓名，性别，院系，专业，学号
+	const { id,headImg,name,sex,department,major,stuNumber } = event;
+	//2.获取用户头像，判断是否更新
+	//2.根据用户id，更新上面信息
+	var db = uniCloud.database();
+	return await db.collection('user').doc(id).update({headImg,name,sex,department,major,stuNumber})
+};
